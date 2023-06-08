7fbe7667b20703654f06a2b5a9a3cc232c7c2729
Aayush Dongol <ayussh222dongol@gmail.com>
2023-06-08 20:57:38 +0545

    nth

--- frontend/src/app/components/QuantitySelector.tsx
+++ frontend/src/app/components/QuantitySelector.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { IoIosAdd } from "react-icons/io"
 import { HiOutlineMinusSm } from "react-icons/hi"
 const QuantitySelector = () => {
+  console.log(null)
   return (
     <div className='flex items-center gap-2 border border-[black] w-[100px]'>
       <IoIosAdd className='h-3 w-3' />
