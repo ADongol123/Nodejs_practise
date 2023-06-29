57e19abbacda577f00876dc50d58849fd0401c1b
Aayush Dongol <ayussh222dongol@gmail.com>
2023-06-29 17:24:20 +0545

    author api created

--- backend/routes/authorRoutes.js
+++ backend/routes/authorRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { postAuthor, getAuthor } from "../controllers/authorController.js";
 
 const router = express.Router();
-
+console.log(router);
 router.route("/postAuthor").post(postAuthor);
 router.route("/getAuthor").get(getAuthor);
 
